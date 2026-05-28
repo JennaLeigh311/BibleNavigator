717d692f4afc15f1e0e93146b4e38d6308e2265d
Jenna <uuujanna@gmail.com>
2026-05-28 11:25:13 -0700

    Make new xcode project

---
+++ BibleNavigator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BibleNavigator
+//
+//  Created by Jenna Bunescu on 5/28/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
